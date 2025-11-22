c46a488b90c28285af789a07859b8a863f75f0b9
RitaAjdinii <ritaajdini722@gmail.com>
2025-11-23 00:29:51 +0100

    create the contact form on the front

--- ContactUs.php
+++ ContactUs.php
@@ -9,6 +9,17 @@
 <body>
     <?php include "header.php";?>
     <main>
+        <section class="contact-us-section">
+            <div class="contact-us-container">
+                <h2>Contact Us</h2>
+                <form action="" class="contact-form">
+                    <input type="text" placeholder="Your Name" >
+                    <input type="email" placeholder="Your email">
+                    <textarea  name="send-message" id="">Send Message</textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </section>
     </main>
     <?php include "footer.php";?>
      <script src="navbar.js">
